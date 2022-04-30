3ac0ab56ff0a4c2bdfed917cb6040aacd155229f
James Sullivan <jsullivan2847@gmail.com>
2022-04-30 19:18:39 -0400

    added some js DOM manipulation to html and css

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+const mainEl = document.querySelector('main');
+const topMenuEl = document.getElementById("top-menu");
+
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+mainEl.classList.add("flex-ctr");
+
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+menuLinks.forEach(function(link){
+   newLink = document.createElement("a");
+   newLink.setAttribute("href", link.href);
+   newLink.innerHTML = link.text;
+   topMenuEl.append(newLink);
+});
